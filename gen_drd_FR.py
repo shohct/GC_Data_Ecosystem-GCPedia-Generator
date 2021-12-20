878d5c3214fa1fa53c494f71561a9e058476046b
rmk07 <78704641+rmk07@users.noreply.github.com>
2021-12-20 10:51:23 -0600

    Latest Version of DERD - Both French and English
    Two sets of files - one for French webpage, one for English

--- gen_drd_FR.py
+++ gen_drd_FR.py
@@ -11,12 +11,12 @@ Element = namedtuple("Element", ["subtype", "name", "url", "desc"])  # contains 
 # List of Categories from Airtable
 # Sub-categories are categories that are listed in Airtable
 CATEGORIES = {
-    "Communities": {"Committees", "Communities","Working Groups"},
-    "Data Glossaries": {"Data Glossaries", "Other", "Top-20 Data terms"},
-    "Learning Resources": {"Data Glossaries", "Documents", "Learning Resources", },
-    "Organizations and Teams": {"Organizations", "Teams"},
-    "Projects/Initiatives": {"Projects/Initiatives"},
-    "Policy Instruments": {"Policy Instruments"},  
+    "Communautés ": {"Comités", "Communautés ", "Groupes de travail"},
+    "Glossaires de données": {"Glossaires de données", "Autre", "Les 20 principaux termes relatifs aux données"},
+    "Ressources d'apprentissage": {"Glossaires de données", "Documents", "Ressources d'apprentissage", },
+    "Organisations avec Équipes": {"Organisations", "Équipes"},
+    "Projets / initiatives": {"Projets / initiatives"},
+    "Instruments de politique": {"Instruments de politique"},  
 }
 
 
@@ -25,8 +25,8 @@ def df_to_elem(group) -> list:
 
     # Nested Function / Helper function - creates an element as long as the url is not na
     def make_elem(r):
-        url = r["URL"] if not pd.isna(r["URL"]) else None  # Add a url if the url field is not empty
-        description = r["Description"] if not pd.isna(r["Description"]) else None
+        url = r["French URL"] if not pd.isna(r["French URL"]) else None  # Add a url if the url field is not empty
+        description = r["French Description"] if not pd.isna(r["French Description"]) else None
         element = Element(r["Sub-Type"], r["Entity Name"], url, description)
         return element  # Return an element with the name from dictionary and url
 
@@ -39,31 +39,37 @@ def df_to_elem(group) -> list:
 def load_data(path) -> dict:
     # Initialize dataframe
     df = (
-        pd.read_csv(path, usecols=["Label", "Type", "SubType", "URL", "Not4DERD",
-                                   "Short Description Eng"])  # read in columns from file
-        .dropna(subset=["Label", "SubType", "Type"])  # Discard Columns with NA values at Entity full name or Type
+        pd.read_csv(path, usecols=["French Entity Full Name", "Type", "SubType", "French URL", "Not4DERD",
+                                   "French Description"])  # read in columns from file
+        .dropna(subset=["French Entity Full Name", "SubType", "Type"])  # Discard Columns with NA values at Entity full name or Type
         .reset_index(drop=True)  # Discard Airtable indexing
     )
+
+    # Call translator
+    df = translate_types(df)
+    df.drop("SubType", axis=1, inplace=True)
+    df.drop("Type", axis=1, inplace=True)
+
+    # Remove any entires that do not have a translation
+    df = df.dropna(subset=[ "SubType FR", "Type FR"])
 
     df = df[df.Not4DERD != "checked"]  # Discard entries that are Not4DERD
     df.drop("Not4DERD", axis=1, inplace=True)  # Remove column "Entity Full Name"
     # Clean names and remove unwanted chars
-    df["Type"] = df["Type"].str.strip()
-    df["Entity Name"] = df["Label"].str.strip()
-    df["Sub-Type"] = df["SubType"].str.strip()
-    df["Description"] = df["Short Description Eng"].str.strip()
-    df["Description"] = df["Description"].str.replace('\n', '')
-    df["URL"] = df["URL"].str.strip()
+    df["Type"] = df["Type FR"].str.strip()
+    df["Entity Name"] = df["French Entity Full Name"].str.strip()
+    df["Sub-Type"] = df["SubType FR"].str.strip()
+    df["French Description"] = df["French Description"].str.strip()
+    df["French Description"] = df["French Description"].str.replace('\n', '')
+    df["French URL"] = df["French URL"].str.strip()
 
     # Remove column "Entity Full Name" and column "Entity sub-type"
-    df.drop("Label", axis=1, inplace=True)
-    df.drop("SubType", axis=1, inplace=True)
-    df.drop("Short Description Eng", axis=1, inplace=True)
+    df.drop("French Entity Full Name", axis=1, inplace=True)
 
     df.sort_values(by=['Type', 'Sub-Type', 'Entity Name'], inplace=True)  # Sort alphabetically by type, then sub-type, then name
     df.reset_index(drop=True, inplace=True)  # Reset dataframe indices
     
-    new_rows = {"Type": [],  "URL": [], "Entity Name": [], "Sub-Type": [], "Description": []}  # Create dict entry with type, subtype, description, url, and entity name
+    new_rows = {"Type": [],  "French URL": [], "Entity Name": [], "Sub-Type": [], "French Description": []}  # Create dict entry with type, subtype, description, url, and entity name
 
     # Fix the issue that some items have more than none sub-type and should be listed twice
     for i, row in df.iterrows():  # Go through each row in the dataframe
@@ -74,9 +80,9 @@ def load_data(path) -> dict:
 
             for extra_type in types[1:]:  # Go through each type and add a new entry for each type
                 new_rows["Type"].append(row["Type"])  # Add a new row with the Type, Sub-Type, Url, and Entity
-                new_rows["URL"].append(row["URL"])
+                new_rows["French URL"].append(row["French URL"])
                 new_rows["Entity Name"].append(row["Entity Name"])
-                new_rows["Description"].append(row["Description"])
+                new_rows["French Description"].append(row["French Description"])
                 new_rows["Sub-Type"].append(extra_type)
 
             df.iloc[i]["Sub-Type"] = types[0]  # Add to dataframe the index of the current entry
@@ -121,6 +127,49 @@ def gen_url(item) -> str:
         return f"#{item.replace('/', '.2F')}"
     return f"#{parse.quote(item.replace(' ', '_'), safe='')}"
 
+# Translates entity types and subtypes automatically using extra csv file
+def translate_types(data_frame):
+    # get the file containing translations
+    type_names = pd.read_csv("Entity Types-Grid view.csv", usecols=["Entity Type Eng", "Entity Type FR"] )
+    type_names = type_names.set_index('Entity Type Eng')
+
+    # Make the translations into a dictionary for types and subtypes
+    type_dictionary = type_names.to_dict()
+    type_dictionary = type_dictionary['Entity Type FR']
+
+    subtype_names = pd.read_csv("Entity sub-type-Grid view.csv", usecols=["Entity sub-type", "Entity sub-type FR"] )
+    subtype_names = subtype_names.set_index('Entity sub-type')
+
+    subtype_dictionary = subtype_names.to_dict()
+    subtype_dictionary = subtype_dictionary['Entity sub-type FR']
+
+    # Create a list containing the type translations
+    type_names_fr = []
+    for type in data_frame["Type"]:
+        type_names_fr.append(type_dictionary[type])
+
+    # create a list containing subtype translations
+    subtype_names_fr = []
+    for subtype in data_frame["SubType"]:
+        subtypes = [t.strip() for t in subtype.split(",")] # If an entry has more than one subtype, translate both
+        if (len(subtypes)) > 1:
+            subtypes_fr = ""
+            count = 0
+            for type in subtypes:
+                if count < len(subtypes)-1:
+                    subtypes_fr += subtype_dictionary[type] + ","
+                else: 
+                    subtypes_fr += subtype_dictionary[type]
+                count = count + 1
+            subtype_names_fr.append(subtypes_fr)
+        else:
+            subtype_names_fr.append(subtype_dictionary[subtype])
+
+    # Add translations into the data_frame
+    data_frame.insert(0, "Type FR", type_names_fr)
+    data_frame.insert(0, "SubType FR", subtype_names_fr)
+
+    return data_frame
 
 #  Creates and initializes parser
 def make_parser() -> argparse.ArgumentParser:
@@ -144,7 +193,7 @@ def make_parser() -> argparse.ArgumentParser:
         "-o",
         dest="output",
         type=argparse.FileType('w', encoding='UTF-8'),
-        default="output_template.txt",
+        default="output_template_FR.txt",
         help="Destination file to write to. Defaults to stdout.",
     )
 
@@ -163,7 +212,7 @@ def main():
     # Import data and clean it up for Jinja
     env.globals.update(format_link_text=format_link_text, gen_url=gen_url, data=data)
 
-    template = env.get_template("drd_two_col.j2")  # Load the Jinja2 template with loader and return it
+    template = env.get_template("drd_two_col_FR.j2")  # Load the Jinja2 template with loader and return it
 
     # Print template to text file which can then be copied into GCPedia
     args.output.write(template.render())
